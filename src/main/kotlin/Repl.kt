7b696e4929b9c8cbc68dc28c76e536e1d2c2a70b
Daniil Demidko <demidko@farpost.com>
2021-07-01 05:32:38 +1000

    fix

--- src/main/kotlin/Repl.kt
+++ src/main/kotlin/Repl.kt
@@ -5,13 +5,13 @@ import javax.script.ScriptEngineManager
 
 fun main(args: Array<String>) {
   check(args.size == 1) { "Invalid args. The utility only accepts one file path." }
-  val ndjson = File(args.first()).ndjson()
+  val cache = File(args.first()).ndjson()
   with(ScriptEngineManager().getEngineByExtension("kts")) {
     print(":) ")
     `in`.reader().forEachLine {
       try {
         println()
-        for (batch in eval("ndjson.collect($it)") as List<List<JsonNode>>) {
+        for (batch in eval("cache.collect($it)") as List<List<JsonNode>>) {
           batch.forEach(::println)
           println()
         }
