90173f5e913e4a1ff63e0c7e4618218fce653ba8
Daniil Demidko <demidko@farpost.com>
2021-07-02 09:51:03 +1000

    wip

--- src/main/kotlin/Query.kt
+++ src/main/kotlin/Query.kt
@@ -1,92 +1,45 @@
-import com.fasterxml.jackson.databind.JsonNode
+typealias Query = MutableList<Any>.(Any) -> Unit
 
-interface Query {
-  fun apply(list: MutableList<JsonNode>, el: JsonNode)
-  fun build(list: List<JsonNode>): List<Any>
-}
-
-interface Mapper : Query {
-
-  override fun build(list: List<JsonNode>) = map(list)
-
-  fun map(list: List<Any>): List<Any>
-}
-
-fun interface Mixer : Query {
-  override fun build(list: List<JsonNode>) = list
-}
-
-fun map(mapper: JsonNode.() -> Any) = object : Mapper {
-
-  override fun map(list: List<Any>) = (list as List<JsonNode>).map(mapper)
-
-  override fun apply(list: MutableList<JsonNode>, el: JsonNode) {
-    list.add(el)
+fun top(limit: Int): Query = {
+  if (size < limit) {
+    add(it)
   }
 }
 
-infix fun Mapper.map(mapper: Any.() -> Any) = object: Mapper {
-  override fun apply(list: MutableList<JsonNode>, el: JsonNode) {
-    TODO("Not yet implemented")
+fun Query.top(limit: Int): Query = {
+  while (size > limit) {
+    removeLast()
   }
 }
 
-
-{
-  map(it).map(mapper)
-}
-
-infix fun Mixer.map(mapper: JsonNode.() -> Any) = Mapper {
-  build(it as List<JsonNode>).map(mapper)
-}
-
-fun top(limit: Int) = Mixer { list, el ->
-  if (list.size < limit) {
-    list.add(el)
+fun where(filter: Any.() -> Boolean): Query = {
+  if (it.filter()) {
+    add(it)
   }
 }
 
-infix fun Mixer.top(limit: Int) = Mixer { list, el ->
-  apply(list, el)
-  while (list.size > limit) {
-    list.removeLast()
+fun Query.where(filter: Any.() -> Boolean): Query = {
+  if (it.filter()) {
+    this@where(it)
   }
 }
 
-infix fun Mapper.top(limit: Int) = Mapper { map(it).take(limit) }
-
-fun where(filter: JsonNode.() -> Boolean) = Mixer { list, el ->
-  if (el.filter()) {
-    list.add(el)
-  }
+fun order(comparator: Comparator<Any>): Query = {
+  add(it)
+  sortWith(comparator)
 }
 
-infix fun Mixer.where(filter: JsonNode.() -> Boolean) = Mixer { list, el ->
-  if (el.filter()) {
-    apply(list, el)
-  }
+fun Query.order(comparator: Comparator<Any>): Query = {
+  this@order(it)
+  sortWith(comparator)
 }
 
-infix fun Mapper.where(f: Any.() -> Boolean) = Mapper { it.filter(f) }
-
-fun order(comparator: Comparator<JsonNode>) = Mixer { list, el ->
-  list.add(el)
-  list.sortWith(comparator)
+fun map(mapper: Any.() -> Any): Query = {
+  add(it.mapper())
 }
 
-infix fun Mixer.order(comparator: Comparator<JsonNode>) = Mixer { list, el ->
-  apply(list, el)
-  list.sortWith(comparator)
+fun Query.map(mapper: Any.() -> Any): Query = {
+  this@map(it)
+  this.replaceAll(mapper)
 }
 
-fun List<JsonNode>.collect(vararg queries: Query): List<List<Any>> {
-  val results = queries.map { it to mutableListOf<JsonNode>() }
-  for (node in this) {
-    for ((query, result) in results) {
-      query.apply(result, node)
-    }
-  }
-  return results.map { (query, result) ->
-    query.build(result)
-  }
-}
